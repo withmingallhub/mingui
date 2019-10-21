9e7a703e58c878c9628074b9a68b28ddd22a81d8
withmingallhub <40680331+withmingallhub@users.noreply.github.com>
2019-10-21 20:42:45 +0800

    ui

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -26,9 +26,9 @@ const webpackConfig = merge(baseWebpackConfig, {
   devtool: config.build.productionSourceMap ? config.build.devtool : false,
   output: {
     path: config.build.assetsRoot,
-    publicPath:config.build.assetsPublicPath,
     // filename: utils.assetsPath('js/[name].[chunkhash].js'),
     // chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    publicPath:config.build.assetsPublicPath,
     filename: 'ming-components.min.js',
     library:'ming-components',
     libraryTarget:'umd',
