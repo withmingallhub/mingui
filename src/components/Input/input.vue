1b928917edd7ed703a5bc44edb7ec465a3215a00
withmingallhub <40680331+withmingallhub@users.noreply.github.com>
2019-10-22 20:20:07 +0800

    input

--- src/components/Input/input.vue
+++ src/components/Input/input.vue
@@ -1,11 +1,57 @@
 <template>
     <div>
-        <div>这是组件，封装的组件</div>
+        <input 
+        ref="zypcInput"
+        @focus="zypcFocusInput"
+        @blur="zypcBlurInput"
+        :value="value"
+        @input="input"
+        :placeholder="placeholder" 
+        :style="{
+            backgroundColor:backgroundColor,
+            width:width,
+        }" 
+        class="zypc-input-first" 
+        type="text"> 
     </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            
+        }
+    },
+    props:["backgroundColor","placeholder","width","value"],
+    mounted() {
+
+    },
+    methods: {
+        // 获得焦点为input标签添加边框
+        zypcFocusInput(){
+            this.$refs.zypcInput.style.border = '1px solid rgb(3,169,244)'
+        },
+        // 失去焦点将input标签的边框设置为默认值
+        zypcBlurInput(){
+            this.$refs.zypcInput.style.border = '1px solid rgb(150,150,150)'
+        },
+        // 实现v-modle的功能
+        input(){
+            this.$emit('input',event.target.value)
+        }
+    },
 }
 </script>
+
+<style lang="">
+.zypc-input-first {
+    border: 1px solid rgb(150, 150, 150);
+    border-radius: 10px;
+    outline: none;
+    padding-left: 5px;
+    height: 25px;
+    width: 200px;
+    margin: 6px;
+}
+</style>
